03c82ff52a64e8d2d5e520bbc976d9dcbd0bca46
Blookers <blookyl305@gmail.com>
2022-06-12 22:22:24 +1000

    Add Tooltip component

---
+++ components/Tooltip.jsx
@@ -0,0 +1,68 @@
+import { styled, keyframes } from '@stitches/react';
+import * as TooltipPrimitive from '@radix-ui/react-tooltip';
+
+import ThemeContext from '../contexts/theme';
+
+import { getTheme } from '../lib/themes';
+const slideUpAndFade = keyframes({
+    '0%': { opacity: 0, transform: 'translateY(2px)' },
+    '100%': { opacity: 1, transform: 'translateY(0)' }
+});
+
+const slideRightAndFade = keyframes({
+    '0%': { opacity: 0, transform: 'translateX(-2px)' },
+    '100%': { opacity: 1, transform: 'translateX(0)' }
+});
+
+const slideDownAndFade = keyframes({
+    '0%': { opacity: 0, transform: 'translateY(-2px)' },
+    '100%': { opacity: 1, transform: 'translateY(0)' }
+});
+
+const slideLeftAndFade = keyframes({
+    '0%': { opacity: 0, transform: 'translateX(2px)' },
+    '100%': { opacity: 1, transform: 'translateX(0)' }
+});
+
+const StyledContent = styled(TooltipPrimitive.Content, {
+    color: '$primaryColor',
+    border: '1px solid $secondaryBorder',
+    padding: '.5rem .75rem',
+    fontSize: '.75rem',
+    boxShadow: '0 0 4px 2px rgba(0, 0, 0, .1)',
+    lineHeight: 1,
+    fontFamily: 'Nunito',
+    background: '$primaryBackground',
+    borderRadius: 6,
+    '@media (prefers-reduced-motion: no-preference)': {
+        animationDuration: '400ms',
+        animationTimingFunction: 'cubic-bezier(0.16, 1, 0.3, 1)',
+        animationFillMode: 'forwards',
+        willChange: 'transform, opacity',
+        '&[data-state="delayed-open"]': {
+            '&[data-side="top"]': { animationName: slideDownAndFade },
+            '&[data-side="right"]': { animationName: slideLeftAndFade },
+            '&[data-side="bottom"]': { animationName: slideUpAndFade },
+            '&[data-side="left"]': { animationName: slideRightAndFade }
+        }
+    }
+});
+
+const StyledArrow = styled(TooltipPrimitive.Arrow, {
+    fill: '$secondaryBorder'
+});
+
+export const Root = TooltipPrimitive.Root;
+export const Arrow = StyledArrow;
+export const Trigger = TooltipPrimitive.Trigger;
+export const Provider = TooltipPrimitive.Provider;
+
+export function Content(props) {
+    return (
+        <ThemeContext.Consumer>{({ theme }) => {
+            return <StyledContent className={getTheme(theme)} {...props}>
+                {props.children}
+            </StyledContent>
+        }}</ThemeContext.Consumer>
+    );
+};
---
+++ index.js
@@ -0,0 +1,33 @@
+export { default as App } from './components/App';
+export { default as Header } from './components/Header';
+
+export { default as Link } from './components/Link';
+
+export { default as Navigation } from './components/Navigation';
+export { default as SideNavigation } from './components/SideNavigation';
+export { default as NavigationItem } from './components/SideNavigation/Item';
+
+export { default as Tabs } from './components/Tabs';
+export { default as TabItem } from './components/Tabs/Item';
+
+export { default as Typography } from './components/Typography';
+export { default as TextHeader } from './components/Typography/Header';
+
+export { default as Alert } from './components/Alert';
+export { default as BasicSpinner } from './components/BasicSpinner';
+export { default as Button } from './components/Button';
+export { default as Card } from './components/Card';
+export * as Dialog from './components/Dialog';
+export { default as Divider } from './components/Divider';
+export * as DropdownMenu from './components/DropdownMenu';
+export { default as Grid } from './components/Grid';
+export { default as Image } from './components/Image';
+export { default as Main } from './components/Main';
+export { default as Markdown } from './components/Markdown';
+export { default as Portal } from './components/Portal';
+export { default as Range } from './components/Range';
+export { default as Spinner } from './components/Spinner';
+export { default as Stepper } from './components/Stepper';
+export { default as Table } from './components/Typography';
+export { default as Toaster } from './components/Typography';
+export { default as WindowButtons } from './components/Typography';
